66fa82bbce0490d595753e81f97340d4aecd57ea
Sebastian Pasara <sebapasa10@gmail.com>
2025-11-16 17:19:36 -0500

    Add files via upload

---
+++ passwordGenerator.py
@@ -0,0 +1,18 @@
+import random
+def password_generator():
+    lowerchars = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+    upperchars = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+
+    specialchars = ['!','&','*','@','#','$','%','^','?','_','+','=','-','~']
+    numericchars = ['1','2','3','4','5', '6','7','8','9','0']
+
+    password = (random.choice(lowerchars) + random.choice(lowerchars) + 
+                random.choice(upperchars) + random.choice(upperchars) +
+                random.choice(specialchars) + random.choice(specialchars) +
+                random.choice(numericchars) + random.choice(numericchars))
+
+    password = password + password + password
+
+    return password
+
+print("Your password is: " + password_generator())
